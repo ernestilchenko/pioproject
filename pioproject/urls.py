8338646fbbda4783d80679a485809d8993ef9ee6
ernestilchenko <7777erik777@gmail.com>
2024-06-19 15:32:36 +0200

    error correction

--- pioproject/urls.py
+++ pioproject/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
         path('admin/', admin.site.urls),
-        path('pio/', include('pio_site.urls')),
+        path('pio/', include('piosite.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
