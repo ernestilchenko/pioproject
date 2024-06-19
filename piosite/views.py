f58e1164debc0b41b1fcd711e7d24601736b7338
Ajeszny <245107@edu.p.lodz.pl>
2024-06-19 14:54:08 +0200

    Added login vew

--- piosite/views.py
+++ piosite/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+
+from piosite.forms import RegisterForm, ProfileForm
+from piosite.models import Course, Favorite, Faculty, Speciality, Assignment, Profile
+
 
 # Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('faculty_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, "html/logowanie.html", {"form": form})
